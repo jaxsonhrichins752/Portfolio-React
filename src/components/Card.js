9d94bf51c37967b5d09e2e6574d5c39ab168eb81
jaxsonhrichins752 <jaxson.h.richins.752@gmail.com>
2026-03-18 01:33:36 +0000

    Fixed card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Heading, HStack, Image, Text, VStack, CardBody } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -8,11 +8,13 @@ const Card = ({ title, description, imageSrc }) => {
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
   return (
-    <VStack spacing={6}>
-      <Image src={imageSrc} alt={title} />
-      <Heading as='h3' size='2xl'>{title}</Heading>
-      <Text fontSize='lg'>{description}</Text>
-    </VStack>
+    <CardBody>
+      <VStack spacing={6}>
+        <Image src={imageSrc} alt={title} />
+        <Heading as='h3' size='2xl'>{title}</Heading>
+        <Text fontSize='lg'>{description}</Text>
+      </VStack>
+    </CardBody>
   );
 };
 
